9a1f060179d890feb4669dd73448bc011fe67df0
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2026-01-03 20:52:18 -0300

    Configuracion de ruteo entre componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Servicios from './components/Servicios'
+import Metodologia from './components/Metodologia';
 
 function App() {
   return (
     <>
-      <div className='bg-linear-to-b from-[#4c1d95] via-[#2e1065] to-gray-900 min-h-screen text-violet-400'>
+      <div className='bg-linear-to-b from-[#4c1d95] via-[#2e1065] to-gray-900 min-h-screen text-violet-400 scroll-smooth'>
         <Navbar />
           <Routes>
-            <Route path='/' element={<Hero />} />
+            <Route path='/' element={   
+              <>
+                <Hero />
+                <Servicios />
+                <Metodologia />
+              </>
+            } />
           </Routes>
       </div>
     </>
